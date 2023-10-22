f08b0c8b2014254477981bfb250ff74ab2a4c38a
dfrytek <dominikaaaxx@gmail.com>
2023-10-22 14:04:36 +0200

    ZMineiłam plik zadanie.py

--- zadanie.py
+++ zadanie.py
@@ -1,2 +1,4 @@
 miasto="Kraków"
+miasto2="Bochnia" # podaję kolejne miasto w małopolsce
 print("Przykład miasta w województwie małopolskim:", miasto)
+print("Przykład innego miasta w województwie małopolskim:", miasto2)

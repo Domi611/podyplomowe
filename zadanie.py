317dabcf134c923d2e962c8166b5b04bb43f39c4
dfrytek <dominikaaaxx@gmail.com>
2023-10-22 14:10:29 +0200

    Poprawiłam literówkę

--- zadanie.py
+++ zadanie.py
@@ -1,4 +1,4 @@
 miasto="Kraków"
-miasto2="Bochnia" # podaję kolejne miasto w małopolsce
+miasto2="Bochnia" #podaję kolejne miasto w małopolsce
 print("Przykład miasta w województwie małopolskim:", miasto)
 print("Przykład innego miasta w województwie małopolskim:", miasto2)
